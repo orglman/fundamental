a0d510c22359af93c2adbead32ff0782c1a477b2
Tobias @ Orgelman Entertainment <39733563+orglman@users.noreply.github.com>
2018-11-08 10:03:25 +0100

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -536,5 +536,36 @@ namespace orgelman\functions {
 
         return (substr($haystack, -$length) === $needle);
       }
+     
+      public function luhnCreate($s) {
+        // Add a zero check digit
+        $s = $s . '0';
+        $sum = 0;
+        // Find the last character
+        $i = strlen($s);
+        $odd_length = $i % 2;
+        // Iterate all digits backwards
+        while ($i-- > 0) {
+            // Add the current digit
+            $sum+=$s[$i];
+            // If the digit is even, add it again. Adjust for digits 10+ by subtracting 9.
+            ($odd_length == ($i % 2)) ? ($s[$i] > 4) ? ($sum+=($s[$i] - 9)) : ($sum+=$s[$i]) : false;
+        }
+        return (10 - ($sum % 10)) % 10;
+      }
+      public function luhnValidate($number) {
+        $sum = 0;
+        $numDigits = strlen($number) - 1;
+        $parity = $numDigits % 2;
+        for ($i = $numDigits; $i >= 0; $i--) {
+            $digit = substr($number, $i, 1);
+            if (!$parity == ($i % 2)) {
+                $digit <<= 1;
+            }
+            $digit = ($digit > 9) ? ($digit - 9) : $digit;
+            $sum += $digit;
+        }
+        return (0 == ($sum % 10));
+      } 
    }
 }
