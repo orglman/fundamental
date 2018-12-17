b0e29c2b1a66278cec880df590480ebaaea54cf5
Tobias @ Orgelman Entertainment <39733563+orglman@users.noreply.github.com>
2018-12-17 01:19:51 +0100

    Create randomString.trait.php

---
+++ src/traits/randomString.trait.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * @package orglman/fundamental
+ * @link    https://github.com/orglman/fundamental/
+ * @author  Tobias Jonson <git@orgelman.systems>
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (GNU GPL)
+ */
+ 
+trait randomString {
+  function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+  }
+}
