360542468c671420b3ff50fddd4e05422e677e60
Tobias @ Orgelman Entertainment <39733563+orglman@users.noreply.github.com>
2018-12-17 12:22:29 +0100

    Create stringSearch.trait.php

---
+++ src/traits/stringSearch.trait.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * @package orglman/fundamental
+ * @link    https://github.com/orglman/fundamental/
+ * @author  Tobias Jonson <git@orgelman.systems>
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (GNU GPL)
+ */
+ 
+namespace orgelman\functions\traits {
+  trait stringSearch {
+    function startsWith($haystack, $needle) {
+      $length = strlen($needle);
+      return (substr($haystack, 0, $length) === $needle);
+    }
+    function endsWith($haystack, $needle) {
+      $length = strlen($needle);
+      if ($length == 0) {
+        return true;
+      }
+    }
+  }
+}
