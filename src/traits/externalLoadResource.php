df0965a77220d7875b908034cc7ebd215e447a1b
Tobias @ Orgelman Entertainment <39733563+orglman@users.noreply.github.com>
2018-12-18 12:27:12 +0100

    Create externalLoadResource.php

---
+++ src/traits/externalLoadResource.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @package orglman/fundamental
+ * @link    https://github.com/orglman/fundamental/
+ * @author  Tobias Jonson <git@orgelman.systems>
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (GNU GPL)
+ */
+ 
+namespace orgelman\functions\traits {
+  trait externalLoadResource {
+      
+  }
+}
