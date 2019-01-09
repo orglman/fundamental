9d86895913cc43c75372dc674f3dc14f56bc71ad
Tobias @ Orgelman Entertainment <39733563+orglman@users.noreply.github.com>
2019-01-09 01:10:09 +0100

    Update security.class.php

--- src/security.class.php
+++ src/security.class.php
@@ -7,26 +7,35 @@
  */
 namespace orgelman\security {
    class encrypt {
-      use \orgelman\functions\traits\stringConvert;
       use \orgelman\functions\traits\stringHTMLEntities;
+      use \orgelman\functions\traits\stringConvert;
       use \orgelman\functions\traits\stringRandom;
       use \orgelman\functions\traits\stringSearch;
       
-      private $cipher_algorithm = '';
-      private $cipher_method = 'AES-256-CBC';
+      private $cipher_algorithm     = '';
+      private $cipher_method        = 'AES-256-CBC';
       
       private $delimiter_characters = '==00==';
+      private $delimiter_character  = '2';
+      private $delimiter_breaks     = '3';
+      private $delimiter_gz         = '4';
       
-      private $compress = false;
+      private $compress             = true;
       
-      public function __construct($cipher_algorithm = 'sha256') {
+      public function __construct($compress = true, $cipher_algorithm = 'sha256') {
          if(!in_array($cipher_algorithm, hash_algos())) {
             trigger_error('Hashing algorithm invalid', E_USER_ERROR); 
             exit();
          }
-         $this->delimiter_characters = $this->textToBinary($this->delimiter_characters);
+         $this->delimiter_characters = $this->textToBinary($this->delimiter_characters, null);
          
          $this->cipher_algorithm = $cipher_algorithm;
+         
+         if(((function_exists('gzdecode')) && (function_exists('gzencode'))) && ($compress == true)) {
+            $this->compress = true;
+         } else {
+            $this->compress = false;
+         }
       }
       public function __destruct() {
          
@@ -41,13 +50,10 @@ namespace orgelman\security {
       
       
       public function encrypt($str, $key, $method = '') {
-         $str = $this->convertToHTMLEntities($str);
-         $key = $this->convertToHTMLEntities($key);
-         
          if($method == '') {
             $method = $this->cipher_method;
          }
-         $return = array('decrypted' => addslashes(trim($str)), 'encrypted' => '', 'key' => $key, 'safekey' => '', 'algorithm' => $this->cipher_algorithm, 'method' => $method, 'iv' => '', 'base64_encode' => array(), 'error' => array());
+         $return = array('decrypted' => addslashes(trim($str)), 'encrypted' => '', 'key' => $key, 'safekey' => '', 'algorithm' => $this->cipher_algorithm, 'method' => $method, 'iv' => '', 'base64_encode' => array(), 'error' => array(), 'decryptedlen' => strlen(addslashes(trim($str))), 'encryptedlen' => 0, 'compressedlen' => 0);
 
          if($str == '') {
             $return['error'][] = 'String empty';
@@ -75,37 +81,56 @@ namespace orgelman\security {
          
          $return['iv'] = bin2hex($iv); 
          foreach(str_split($return['decrypted'], 20) as $str) {
-            $return['base64_encode'][] = $this->textToBinary(base64_encode(openssl_encrypt($str, $method, $key, OPENSSL_RAW_DATA, $iv)));
+            $return['base64_encode'][] = $this->textToBinary(base64_encode(openssl_encrypt($str, $method, $key, OPENSSL_RAW_DATA, $iv)), null);
          }
-         $return['encrypted'] = $algorithm. "\n" . implode("\n", $return['base64_encode']) . "\n" . $this->textToBinary($return['iv']);
-         if($this->compress==true) {
-            $return['encrypted'] = gzcompress($return['encrypted'],9);
-         }
+         $return['encrypted'] = $algorithm. PHP_EOL . implode(PHP_EOL, $return['base64_encode']) . PHP_EOL . $this->textToBinary($return['iv']);
+         $return['encrypted'] = str_replace(array(' ',PHP_EOL),array($this->delimiter_character,$this->delimiter_breaks),$this->delimiter_characters.' '.$return['encrypted'].' '.$this->delimiter_characters);
+         $return['encryptedlen'] = strlen($return['encrypted']);
          
-         $return['encrypted'] = $this->delimiter_characters.' '.$return['encrypted'].' '.$this->delimiter_characters;
+         if($this->compress==true) {
+            $return['encryptednotcompressed'] = $return['encrypted'];
+            $return['encrypted'] = addslashes($this->delimiter_gz.utf8_encode(gzencode($return['encrypted'],9)).$this->delimiter_gz);
+            $return['compressedlen'] = strlen($return['encrypted']);
+         }
          
          return $return;
       }
       public function decrypt($str, $key, $method = '') {
-         $key = $this->convertToHTMLEntities($key);
+         if(!is_string($str)) {
+            if((is_array($str)) && (isset($str['encrypted']))) {
+               $str = $str['encrypted'];
+            } else {
+               $return['decrypted'] = trim($str);
+               $return['error'][]   = $str = '#'.__LINE__.': String not valid';
+               trigger_error($str, E_USER_NOTICE); 
+               return $return;
+            }
+         }
+         $str = trim(stripslashes($str));
+         if(($this->compress==true) && (($this->startsWith($str, $this->delimiter_gz)) && ($this->endsWith($str, $this->delimiter_gz)))) {
+            $str = gzdecode(trim(utf8_decode($str),$this->delimiter_gz));
+         }
          if($method == '') {
             $method = $this->cipher_method;
          }
+         $rawencrypted = $str;
+         $str = str_replace(array($this->delimiter_character,$this->delimiter_breaks),array(' ',PHP_EOL),$str);
          
          if((!$this->startsWith($str, $this->delimiter_characters)) || (!$this->endsWith($str, $this->delimiter_characters))) {
-            return array('decrypted' => trim($str));
+            $return['decrypted'] = trim($str);
+            $return['error'][]   = $str = '#'.__LINE__.': String not valid';
+            trigger_error($str, E_USER_NOTICE); 
+            return $return;
          } else if(($this->startsWith($str, $this->delimiter_characters)) && ($this->endsWith($str, $this->delimiter_characters))) {
             $str = trim(substr(substr($str,strlen($this->delimiter_characters)), 0, (0-strlen($this->delimiter_characters))));
          } else {
-            $return['error'][] = 'String not valid';
-            trigger_error('String not valid', E_USER_NOTICE); 
+            $return['error'][] = $str = '#'.__LINE__.': String not valid';
+            trigger_error($str, E_USER_NOTICE); 
             return $return;
          }
 
-         $return = array('decrypted' => '', 'encrypted' => trim($str), 'key' => $key, 'safekey' => '', 'algorithm' => $this->cipher_algorithm, 'method' => $method, 'iv' => '', 'base64_encode' => array(), 'error' => array());
-         if($this->compress==true) {
-            $return['encrypted'] = gzuncompress($return['encrypted']);
-         }
+         $return = array('decrypted' => '', 'encrypted' => trim($str), 'rawencrypted' => $rawencrypted, 'key' => $key, 'safekey' => '', 'algorithm' => $this->cipher_algorithm, 'method' => $method, 'iv' => '', 'base64_encode' => array(), 'error' => array());
+         
          
          if($str == '') {
             $return['error'][] = 'String empty';
@@ -125,7 +150,7 @@ namespace orgelman\security {
          $iv = openssl_random_pseudo_bytes($ivlen);
          $ivlen = strlen(bin2hex($iv)); 
          
-         $decrypts = explode("\n", $return['encrypted']);
+         $decrypts = explode(PHP_EOL, $return['encrypted']);
          
          $iv = hex2bin($this->binaryToText(end($decrypts)));
          $return['iv'] = bin2hex($iv); 
@@ -140,7 +165,7 @@ namespace orgelman\security {
          
          $return['base64_encode'] = $decrypts;
          foreach($return['base64_encode'] as $decrypting) {
-            $return['decrypted'] .= $str = convertFromHTMLEntities(openssl_decrypt(base64_decode($this->binaryToText($decrypting)), $method, $key, OPENSSL_RAW_DATA, $iv));
+            $return['decrypted'] .= $str = openssl_decrypt(base64_decode($this->binaryToText($decrypting)), $method, $key, OPENSSL_RAW_DATA, $iv);
          }
          $return['decrypted'] = stripslashes($return['decrypted']);
          
@@ -154,8 +179,8 @@ namespace orgelman\security {
       
       private $saltMaxLength = 255;
 
-      public function __construct() {
-         $this->encrypt = new encrypt();
+      public function __construct($compress = true) {
+         $this->encrypt = new encrypt($compress);
       }
       public function __destruct() {
          
