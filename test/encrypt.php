ff9ee1186752a05ace3f56ff723814438f495c94
Tobias @ Orgelman Entertainment <39733563+orglman@users.noreply.github.com>
2019-01-09 22:32:09 +0100

    Update encrypt.php

--- test/encrypt.php
+++ test/encrypt.php
@@ -1,6 +1,15 @@
-<?php
-$str              = 'valid';
-$encrypt          = new orgelman\security\encrypt('sha256');
-$encrypted        = $encrypt->encrypt($str, $key, $method = '')['encrypted'];
-
-$decrypted        = $encrypt->decrypt($encrypted, $key, $method = '')['decrypted'];
+<?php 
+/**
+ * @package orglman/fundamental
+ * @link    https://github.com/orglman/fundamental/
+ * @author  Tobias Jonson <git@orgelman.systems>
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (GNU GPL)
+ */
+ 
+namespace orgelman\fundamental\test {
+   final class OFSTest extends TestCase {
+      protected function setUp() {
+        
+      }
+   }
+}
